cf18535eb292effc0309f54821cb77ae16b47d92
JoaoVSerafim <104405510+JoaoVSerafim@users.noreply.github.com>
2022-05-24 15:26:30 -0300

    Exercícios Quartel > Casa

---
+++ CursoemVideo JavaScript/NodeS Testes/teste.js
@@ -0,0 +1,6 @@
+let nome = 'João Serafim'
+let idade = 22
+let média = 7.8
+
+//console.log ('O aluno ' + nome + ' de ' + idade + ' teve a média de ' + média + '!') //Jeito mais "Burro" de se fazer
+console.log (`O Aluno ${nome} tem ${idade} e esta com a média de ${média}`)
